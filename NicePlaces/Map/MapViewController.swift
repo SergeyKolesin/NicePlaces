273b626dddbe0426d0be0f58c5906982dc9eceba
Sergei Kolesin <Sergei_Kolesin@epam.com>
2019-01-14 16:42:07 +0300

    no message

--- NicePlaces/Map/MapViewController.swift
+++ NicePlaces/Map/MapViewController.swift
@@ -8,51 +8,40 @@
 
 import UIKit
 import MapKit
-import CoreLocation
+import RxSwift
+import RxCocoa
 
 class MapViewController: UIViewController
 {
 	@IBOutlet weak var mapView: MKMapView!
-	let locationManager = CLLocationManager()
+	let viewModel = MapViewModel()
+	let disposeBag = DisposeBag()
 	
 	override func viewDidLoad()
 	{
 		super.viewDidLoad()
 		mapView.delegate = self
-		locationManager.delegate = self
-		locationManager.desiredAccuracy = kCLLocationAccuracyBest
-		locationManager.requestWhenInUseAuthorization()
 		mapView.showsUserLocation = true
+
+		Observable.of(viewModel.lat.asObservable(), viewModel.lng.asObservable())
+			.merge()
+			.subscribe { _ in
+				let center = CLLocationCoordinate2D(latitude: self.viewModel.lat.value, longitude: self.viewModel.lng.value)
+				let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+				self.mapView.setRegion(region, animated: true)
+			}
+			.disposed(by: disposeBag)
 	}
 	
 	override func viewWillAppear(_ animated: Bool)
 	{
 		super.viewWillAppear(animated)
-		locationManager.startUpdatingLocation()
+		viewModel.startUpdatingLocation()
 	}
 	
 }
 
 extension MapViewController: MKMapViewDelegate
 {
-	
-}
-
-extension MapViewController: CLLocationManagerDelegate
-{
-	
-	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-		guard let location = locations.last else {return}
-		let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-		let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
-		
-		mapView.setRegion(region, animated: true)
-		locationManager.stopUpdatingLocation()
-		
-	}
-	
-	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-		print("Errors " + error.localizedDescription)
-	}
 	
 }
--- NicePlaces/Map/MapViewModel.swift
+++ NicePlaces/Map/MapViewModel.swift
@@ -7,8 +7,40 @@
 //
 
 import Foundation
+import RxSwift
+//import MapKit
+import CoreLocation
 
-class MapViewModel
+class MapViewModel: NSObject
 {
+	let locationManager = CLLocationManager()
+	var lat = Variable<Double>(0)
+	var lng = Variable<Double>(0)
 	
+	override init()
+	{
+		super.init()
+		locationManager.delegate = self
+		locationManager.desiredAccuracy = kCLLocationAccuracyBest
+		locationManager.requestWhenInUseAuthorization()
+	}
+	
+	func startUpdatingLocation()
+	{
+		locationManager.startUpdatingLocation()
+	}
+}
+
+extension MapViewModel: CLLocationManagerDelegate
+{
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		guard let location = locations.last else {return}
+		lat.value = location.coordinate.latitude
+		lng.value = location.coordinate.longitude
+		locationManager.stopUpdatingLocation()
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+		print("Errors " + error.localizedDescription)
+	}
 }
