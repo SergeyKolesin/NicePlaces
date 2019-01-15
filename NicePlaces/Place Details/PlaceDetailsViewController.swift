434586616f2b0e2eb6375a713cc019286567c013
Sergei Kolesin <Sergei_Kolesin@epam.com>
2019-01-15 18:21:16 +0300

    no message

---
+++ NicePlaces/Place Details/PlaceDetailsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PlaceDetailsViewController.swift
+//  NicePlaces
+//
+//  Created by Sergei Kolesin on 1/15/19.
+//  Copyright © 2019 Sergei Kolesin. All rights reserved.
+//
+
+import UIKit
+
+class PlaceDetailsViewController: UIViewController
+{
+	
+}
---
+++ NicePlaces/Place Details/PlaceDetailsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PlaceDetailsViewModel.swift
+//  NicePlaces
+//
+//  Created by Sergei Kolesin on 1/15/19.
+//  Copyright © 2019 Sergei Kolesin. All rights reserved.
+//
+
+import Foundation
+
+class PlaceDetailsViewModel: NSObject
+{
+	
+}
